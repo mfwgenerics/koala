ab2c76f9b4defabfcda3a1538a1afe10e1fb17a8
djo <damien.j.ohara@gmail.com>
2021-09-04 19:01:32 +0800

    mysql dialect uses expressioncompiler

--- mysql/src/main/kotlin/mfwgenerics/kotq/mysql/MysqlDialect.kt
+++ mysql/src/main/kotlin/mfwgenerics/kotq/mysql/MysqlDialect.kt
@@ -6,6 +6,8 @@ import mfwgenerics.kotq.ddl.TableColumn
 import mfwgenerics.kotq.ddl.built.ColumnDefaultExpr
 import mfwgenerics.kotq.ddl.built.ColumnDefaultValue
 import mfwgenerics.kotq.ddl.diff.SchemaDiff
+import mfwgenerics.kotq.dialect.ExpressionCompiler
+import mfwgenerics.kotq.dialect.ExpressionContext
 import mfwgenerics.kotq.dialect.SqlDialect
 import mfwgenerics.kotq.dsl.literal
 import mfwgenerics.kotq.expr.*
@@ -120,10 +122,30 @@ class MysqlDialect: SqlDialect {
 
     private class Compilation(
         val scope: Scope,
-        val sql: SqlTextBuilder = SqlTextBuilder(IdentifierQuoteStyle.BACKTICKS)
-    ) {
+        override val sql: SqlTextBuilder = SqlTextBuilder(IdentifierQuoteStyle.BACKTICKS)
+    ): ExpressionCompiler {
+        override fun <T : Any> reference(context: ExpressionContext, value: Reference<T>) {
+            compileReference(value)
+        }
+
+        override fun subquery(context: ExpressionContext, subquery: BuiltSubquery) {
+            compileSubqueryExpr(subquery)
+        }
+
+        override fun aggregatable(context: ExpressionContext, aggregatable: BuiltAggregatable) {
+            compileAggregatable(aggregatable)
+        }
+
+        override fun <T : Any> dataTypeForCast(to: DataType<T>) {
+            compileCastDataType(to)
+        }
+
+        override fun window(out: SqlTextBuilder, window: BuiltWindow) {
+            compileWindow(window)
+        }
+
         fun compileReference(name: Reference<*>) {
-            sql.addSql(scope[name])
+            sql.addResolved(scope.resolve(name))
         }
 
         fun compileOrderBy(ordinals: List<Ordinal<*>>) {
@@ -243,87 +265,11 @@ class MysqlDialect: SqlDialect {
             }
         }
 
-        fun exhaustive(value: Any?) { }
-
         fun compileExpr(expr: QuasiExpr, emitParens: Boolean = true) {
-            exhaustive(when (expr) {
-                is OperationExpr<*> -> {
-                    when (expr.type.fixity) {
-                        OperationFixity.PREFIX -> {
-                            sql.parenthesize(emitParens) {
-                                sql.addSql(expr.type.sql)
-                                sql.addSql(" ")
-                                compileExpr(expr.args.single(), false)
-                            }
-                        }
-                        OperationFixity.POSTFIX -> {
-                            sql.parenthesize(emitParens) {
-                                compileExpr(expr.args.single(), false)
-                                sql.addSql(" ")
-                                sql.addSql(expr.type.sql)
-                            }
-                        }
-                        OperationFixity.INFIX -> {
-                            sql.parenthesize(emitParens) {
-                                sql.prefix("", " ${expr.type.sql} ").forEach(expr.args) {
-                                    compileExpr(it)
-                                }
-                            }
-                        }
-                        OperationFixity.APPLY -> {
-                            sql.addSql(expr.type.sql)
-                            sql.parenthesize {
-                                sql.prefix("", ", ").forEach(expr.args) {
-                                    compileExpr(it, false)
-                                }
-                            }
-                        }
-                    }
-                }
-                is Literal<*> -> sql.addLiteral(expr)
-                is Reference<*> -> { compileReference(expr) }
-                is AggregatedExpr<*> -> {
-                    val built = expr.buildAggregated()
-
-                    sql.addSql(built.expr.type.sql)
-                    sql.parenthesize {
-                        sql.prefix("", ", ").forEach(built.expr.args) {
-                            compileAggregatable(it)
-                        }
-                    }
-
-                    // h2 doesn't actually support this - maybe have convert to a CASE
-                    built.filter?.let { filter ->
-                        sql.addSql(" FILTER(WHERE ")
-                        compileExpr(filter, false)
-                        sql.addSql(")")
-                    }
-
-                    built.over?.let { window ->
-                        sql.addSql(" OVER (")
-                        compileWindow(window)
-                        sql.addSql(")")
-                    }
-                }
-                is CastExpr<*> -> {
-                    sql.addSql("CAST")
-                    sql.parenthesize {
-                        compileExpr(expr.of, false)
-                        sql.addSql(" AS ")
-                        compileCastDataType(expr.type)
-                    }
-                }
-                is SubqueryExpr -> compileSubqueryExpr(expr.subquery)
-                is ExprListExpr<*> -> sql.parenthesize {
-                    sql.prefix("", ", ").forEach(expr.exprs) {
-                        compileExpr(it, false)
-                    }
-                }
-                is ComparedQuery<*> -> {
-                    sql.addSql(expr.type)
-                    compileSubqueryExpr(expr.subquery)
-                }
-            })
+            return expr.compile(
+                ExpressionContext(needsParens = emitParens),
+                this
+            )
         }
 
         fun compileRelation(relation: BuiltRelation) {
