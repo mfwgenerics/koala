4595ff6162f08eb3202e7dd6748ab805ab984924
djo <damien.j.ohara@gmail.com>
2021-09-25 16:36:20 +0800

    improve mysql test performance

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -2,11 +2,14 @@
 
 { [ $(id -u) -eq 0 ]; } || { echo "execute as root"; exit; }
 
-docker run --rm --publish 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:8 2> /dev/null;
+docker run --rm --publish 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:8 \
+  --innodb-flush-log-at-trx-commit=0 \ # flush-log* settings seem to make the biggest perf difference
+  --innodb-flush-log-at-timeout=30 \
+  --innodb-stats-persistent=OFF;
 
 SECONDS=0
 
-until mysql -u root -h 127.0.0.1 -pmy-secret-pw 2> /dev/null;
+until mysql -u root -h 127.0.0.1 -pmy-secret-pw;
 do
   ((SECONDS < 120)) || exit;
   sleep 1;
