b790661e8fd38e2476646336c132af0050a5427d
djo <damien.j.ohara@gmail.com>
2021-10-18 22:11:59 +0800

    list version of onDuplicate.set(...)

--- core/src/main/kotlin/io/koalaql/query/fluent/OnDuplicated.kt
+++ core/src/main/kotlin/io/koalaql/query/fluent/OnDuplicated.kt
@@ -12,11 +12,14 @@ interface OnDuplicated {
         update(assignments.asList())
 
     /* Syntax sugar for the common case of wanting to update from the inserted values */
-    fun set(vararg assignments: RelvarColumn<*>): Returningable =
+    fun set(assignments: List<RelvarColumn<*>>): Returningable =
         update(assignments.map {
             @Suppress("unchecked_cast")
             val cast = it as RelvarColumn<Any>
 
             ExprAssignment(cast, Excluded[cast])
         })
+
+    fun set(vararg assignments: RelvarColumn<*>): Returningable =
+        set(assignments.asList())
 }
