c1661d55b4868f2825ef20e0b63c71b236f05c18
djo <damien.j.ohara@gmail.com>
2021-10-22 17:14:44 +0800

    jdbc connectors in testImplementation only

--- mysql/build.gradle.kts
+++ mysql/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     implementation(project(":jdbc"))
 
-    implementation("mysql:mysql-connector-java:8.0.26")
+    testImplementation("mysql:mysql-connector-java:8.0.26")
 
     testImplementation(project(":testing"))
     testImplementation(project(":testing", "testArchive"))
--- postgres/build.gradle.kts
+++ postgres/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     implementation(project(":jdbc"))
 
-    implementation("org.postgresql:postgresql:42.2.23")
+    testImplementation("org.postgresql:postgresql:42.2.23")
 
     testImplementation(project(":testing"))
     testImplementation(project(":testing", "testArchive"))
