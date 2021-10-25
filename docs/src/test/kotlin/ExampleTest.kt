d6b67ab21892da3cfaabd0f2775eb5808815a1cb
Damien J O'Hara <damien.j.ohara@gmail.com>
2021-10-25 17:56:22 +0800

    result set examples under test

--- docs/src/test/kotlin/ExampleTest.kt
+++ docs/src/test/kotlin/ExampleTest.kt
@@ -1,9 +1,15 @@
 import io.koalaql.docs.quickExample
+import io.koalaql.docs.usage.rowExamples
 import kotlin.test.Test
 
 class ExampleTest {
     @Test
     fun `quick example works`() {
         quickExample()
+    }
+
+    @Test
+    fun `results examples`() {
+        rowExamples()
     }
 }
