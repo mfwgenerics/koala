e1b84014a082a0a70c0ba6fc3331921c884e1290
djo <damien.j.ohara@gmail.com>
2022-03-31 18:00:07 +0800

    don't require root for docker scripts

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-{ [ $(id -u) -eq 0 ]; } || { echo "execute as root"; exit; }
-
 # flush-log-* settings make biggest DDL test perf difference
 docker run --rm --publish 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:8 \
   --innodb-flush-log-at-trx-commit=0 \
--- postgres/setup.sh
+++ postgres/setup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-{ [ $(id -u) -eq 0 ]; } || { echo "execute as root"; exit; }
-
 docker run --rm --publish 5432:5432 --name postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres:13
