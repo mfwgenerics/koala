7b837ec8962bc9ad46ab58c3201d0a14cc3394ee
Damien O'Hara <damien.j.ohara@gmail.com>
2022-11-22 00:58:51 +0800

    since mysql 8.0.31+ supports except and intercept, uncomment the test case

--- mysql/src/test/kotlin/MysqlUnionableTests.kt
+++ mysql/src/test/kotlin/MysqlUnionableTests.kt
@@ -10,15 +10,6 @@ class MysqlUnionableTests: UnionableTests() {
     @Test
     fun empty() { }
 
-    override fun `except and intersect`() {
-        /* mysql only supports UNION - no INTERSECT or EXCEPT */
-
-        try {
-            super.`except and intersect`()
-            assert(false)
-        } catch (ignored: JdbcException) {  }
-    }
-
     @Test
     override fun `order by on values`() {
         try {
