bab4fbc4f0adf1eb4bf62c91b9d0489b499511bb
Damien O'Hara <damien.j.ohara@gmail.com>
2022-12-17 11:51:10 +0800

    Gradle upgrade + Mysql fixes (#4)
    * Migrate to gradle 7.6
    * Pin mysql container to 8.0.31
    * Change mysql tests to handle ORDER BY on VALUES regression

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # flush-log-* settings make biggest DDL test perf difference
-docker run --rm --publish 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:8 \
+docker run --rm --publish 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:8.0.31 \
   --innodb-flush-log-at-trx-commit=0 \
   --innodb-flush-log-at-timeout=30 \
   --innodb-stats-persistent=OFF \
