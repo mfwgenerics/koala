f50522f5edf41d3b8bbb6754d1944275b3bf40ad
Sam Gammon <sam@elide.ventures>
2023-04-09 12:26:06 -0700

    chore: add build caching support
    - add Gradle config for remote cache in CI and local
    - add env variable -> secret mapping for API key

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,30 @@ include("h2")
 include("mysql")
 include("postgres")
 include("docs")
+
+val cacheUsername: String? by settings
+val cachePassword: String? by settings
+val cachePush: String? by settings
+val remoteCache = System.getenv("GRADLE_CACHE_REMOTE")?.toBoolean() ?: false
+val localCache = System.getenv("GRADLE_CACHE_LOCAL")?.toBoolean() ?: true
+
+buildCache {
+  local {
+    isEnabled = localCache
+  }
+
+  if (remoteCache) {
+    remote<HttpBuildCache> {
+      isEnabled = remoteCache
+      isUseExpectContinue = true
+      isPush = System.getenv("GRADLE_CACHE_PUSH") == "true" || System.getenv("CI") == "true"
+
+      url = uri(System.getenv("CACHE_ENDPOINT") ?: "https://global.less.build/cache/generic/")
+      credentials {
+        username = "apikey"
+        password = System.getenv("BUILDLESS_APIKEY")
+      }
+    }
+  }
+}
+
