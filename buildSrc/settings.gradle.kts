2bd58836974facc2e933c8b17b0d6a97d21ada82
Sam Gammon <s@mg.sv>
2023-12-05 22:20:57 +0000

    chore: caching for `buildSrc`

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,2 +1,14 @@
 /* this exists to prevent gradle from recursing deep in search of a settings file */
-rootProject.name = "koala"
+
+pluginManagement {
+  repositories {
+    maven("https://gradle.pkg.st")
+    gradlePluginPortal()
+  }
+}
+
+plugins {
+  id("build.less") version("1.0.0-beta9")
+}
+
+rootProject.name = "buildSrc"
