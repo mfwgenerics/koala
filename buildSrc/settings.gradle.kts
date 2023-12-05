d59629c5b76b25a4b5a6dba752dd450eaaa918c4
Sam Gammon <s@mg.sv>
2023-12-05 22:04:03 +0000

    fix: settings root names, upgrade kotlin
    - chore: settings root project names
    - chore: upgrade kotlin → `1.9.21`

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,1 +1,2 @@
 /* this exists to prevent gradle from recursing deep in search of a settings file */
+rootProject.name = "koala"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,8 @@ dependencyResolutionManagement {
   }
 }
 
+rootProject.name = "koala"
+
 gradleEnterprise {
   buildScan {
     termsOfServiceUrl = "https://gradle.com/terms-of-service"
