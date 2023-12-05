c4b5a79c539593decdaf0a554d1c2e25e7a80b4c
Sam Gammon <sam@elide.dev>
2023-12-05 15:01:36 +0000

    chore: update `buildless` plugin (#44)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,10 +4,8 @@ pluginManagement {
   }
 }
 
-import build.less.plugin.settings.buildless
-
 plugins {
-  id("build.less") version("1.0.0-beta1")
+  id("build.less") version("1.0.0-beta8")
   id("com.gradle.enterprise") version("3.13")
   id("org.gradle.toolchains.foojay-resolver-convention") version("0.5.0")
 }
@@ -26,7 +24,3 @@ include("h2")
 include("mysql")
 include("postgres")
 include("docs")
-
-buildless {
-  // the sweet sound of silence
-}
