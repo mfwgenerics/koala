26d91b67255f91b4313ffc5da9c0ada0fd79ae45
Sam Gammon <s@mg.sv>
2023-12-05 22:00:44 +0000

    chore: further ci upgrades
    - chore: simplify actions caching
    - chore: upgrade all actions
    - chore: upgrade gradle → `8.5`
    - chore: upgrade jvm → `21`
    - chore: use embedded kotlin plugin

---
+++ build.gradle.kts
@@ -0,0 +1,3 @@
+plugins {
+    embeddedKotlin() apply false
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,22 @@
 pluginManagement {
   repositories {
-    maven("https://gradle.pkg.st/")
+    maven("https://gradle.pkg.st")
+    gradlePluginPortal()
   }
 }
 
 plugins {
   id("build.less") version("1.0.0-beta9")
-  id("com.gradle.enterprise") version("3.13")
-  id("org.gradle.toolchains.foojay-resolver-convention") version("0.5.0")
+  id("com.gradle.enterprise") version("3.16")
+  id("org.gradle.toolchains.foojay-resolver-convention") version("0.7.0")
+  id("com.gradle.common-custom-user-data-gradle-plugin") version("1.12.1")
+}
+
+dependencyResolutionManagement {
+  repositories {
+    maven("https://maven.pkg.st")
+    mavenCentral()
+  }
 }
 
 gradleEnterprise {
@@ -17,10 +26,18 @@ gradleEnterprise {
   }
 }
 
-include("core")
-include("jdbc")
-include("testing")
-include("h2")
-include("mysql")
-include("postgres")
-include("docs")
+listOf(
+  "core",
+  "jdbc",
+  "testing",
+  "h2",
+  "mysql",
+  "postgres",
+  "docs",
+).forEach {
+  include(it)
+}
+
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("GROOVY_COMPILATION_AVOIDANCE")
